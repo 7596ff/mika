3ebb5053b6f3047c82d7c930b652758bb0618202
Cassandra McCarthy <cassie@7596ff.com>
2017-01-09 09:52:55 -0500

    add getPlayer

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function Mika() {
     this.matches = {
         getMatch: require('./lib/matches/getMatch.js')
     }
+    
+    this.players = {
+        getPlayer: require('./lib/players/getPlayer.js')
+    }
 }
 
 module.exports = Mika
