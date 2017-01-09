e8b484ff85f25f1a93a85c2905417e38940fa460
Brendan McCarthy <brendan10211@gmail.com>
2017-01-09 09:53:08 -0500

    update test.js to include relevant info

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const Mika = require('./index.js')
 
 var mika = new Mika();
 
-test = mika.matches.getMatch('2903478562').then((body) => {
-    console.log(body.match_id)
-}).catch((err) => console.log(err))
+test = mika.players.getPlayer('103637655').then((player) => {
+    console.log(`Solo MMR: ${player.solo_competitive_rank}`)
+    console.log(`Account ID: ${player.profile.account_id}`)
+    console.log(`Name: ${player.profile.personaname}`)
+}).catch((err) => {
+    console.log("err:" + err)
+})
