8fa4a0d626dc5ff61f5b647b67b496e8db338e86
Brendan McCarthy <brendan10211@gmail.com>
2017-01-09 12:59:19 -0500

    add getPlayerCounts, add a relevant test

--- test.js
+++ test.js
@@ -2,10 +2,22 @@ const Mika = require('./index.js')
 
 var mika = new Mika();
 
-test = mika.players.getPlayer('103637655').then((player) => {
+mika.players.getPlayer('103637655').then((player) => {
     console.log(`Solo MMR: ${player.solo_competitive_rank}`)
     console.log(`Account ID: ${player.profile.account_id}`)
     console.log(`Name: ${player.profile.personaname}`)
+}).catch((response) => {
+    console.log(`statusCode: ${response.statusCode}`)
+    console.log(`body: ${response.body}`)
+})
+
+    let leavers = 0
+    for (leaver_stat in counts.leaver_status) {
+        if (leaver_stat != "0") {
+            leavers += counts.leaver_status[leaver_stat].games
+        }
+    }
+    console.log(`\nGames with at least one leaver: ${leavers}`)
 }).catch((response) => {
     console.log(`statusCode: ${response.statusCode}`)
     console.log(`body: ${response.body}`)
