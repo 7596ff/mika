10e0bd9e31e3a46b02fbd6b0f26aca3aeec006f9
Cassandra McCarthy <cassie@7596ff.com>
2017-01-09 12:20:16 -0500

    add getPlayerPeers

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function Mika() {
         getPlayer: require('./lib/players/getPlayer.js'),
         getPlayerWL: require('./lib/players/getPlayerWL.js'),
         getPlayerMatches: require('./lib/players/getPlayerMatches.js'),
-        getPlayerHeroes: require('./lib/players/getPlayerHeroes.js')
+        getPlayerHeroes: require('./lib/players/getPlayerHeroes.js'),
+        getPlayerPeers: require('./lib/players/getPlayerPeers.js')
     }
 }
 
