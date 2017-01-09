4d90d624a3daafb0cba2ce28092d65f72cd3fc08
Brendan McCarthy <brendan10211@gmail.com>
2017-01-09 10:43:46 -0500

    add getPlayerWL

---
+++ util/queryString.js
@@ -0,0 +1,9 @@
+module.exports = (options) => {
+    var str = [];
+    for(var p in options) {
+        if (options.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(options[p]));
+        }
+    }
+    return str.join("&");
+}
