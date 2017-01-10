27dc9c45aa43090018f4bd04d16788bf27c81ced
Brendan McCarthy <brendan10211@gmail.com>
2017-01-09 21:42:32 -0500

    make pretty

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function Mika() {
     this.getMatch = require('./lib/matches/getMatch')
     
     this.players = {
-        getPlayer: require('./lib/players/getPlayer'),
-        getPlayerWL: require('./lib/players/getPlayerWL'),
-        getPlayerMatches: require('./lib/players/getPlayerMatches'),
-        getPlayerHeroes: require('./lib/players/getPlayerHeroes'),
-        getPlayerPeers: require('./lib/players/getPlayerPeers'),
-        getPlayerPros: require('./lib/players/getPlayerPros'),
-        getPlayerRecords: require('./lib/players/getPlayerRecords'),
-        getPlayerCounts: require('./lib/players/getPlayerCounts'),
-        getPlayerHistograms: require('./lib/players/getPlayerHistograms'),
-        getPlayerWardmap: require('./lib/players/getPlayerWardmap'),
-        getPlayerWordcloud: require('./lib/players/getPlayerWordcloud'),
-        getPlayerRatings: require('./lib/players/getPlayerRatings'),
-        getPlayerRankings: require('./lib/players/getPlayerRankings'),
-        postPlayerRefresh: require('./lib/players/postPlayerRefresh')
+        "getPlayer": require('./lib/players/getPlayer'),
+        "getPlayerWL": require('./lib/players/getPlayerWL'),
+        "getPlayerMatches": require('./lib/players/getPlayerMatches'),
+        "getPlayerHeroes": require('./lib/players/getPlayerHeroes'),
+        "getPlayerPeers": require('./lib/players/getPlayerPeers'),
+        "getPlayerPros": require('./lib/players/getPlayerPros'),
+        "getPlayerRecords": require('./lib/players/getPlayerRecords'),
+        "getPlayerCounts": require('./lib/players/getPlayerCounts'),
+        "getPlayerHistograms": require('./lib/players/getPlayerHistograms'),
+        "getPlayerWardmap": require('./lib/players/getPlayerWardmap'),
+        "getPlayerWordcloud": require('./lib/players/getPlayerWordcloud'),
+        "getPlayerRatings": require('./lib/players/getPlayerRatings'),
+        "getPlayerRankings": require('./lib/players/getPlayerRankings'),
+        "postPlayerRefresh": require('./lib/players/postPlayerRefresh')
     }
 
     this.getProPlayers = require('./lib/proPlayers/getProPlayers')
